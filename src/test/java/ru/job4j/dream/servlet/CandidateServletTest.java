babce5fbe71f5d950bdb5454c6cf001cc0a38256
oryngalikarimzhan <oryngalikarimzhan@github.com>
2022-03-06 16:48:08 +0600

    1. Написать тест на PostServlet. [#2518]

---
+++ src/test/java/ru/job4j/dream/servlet/CandidateServletTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.dream.servlet;
+
+import org.junit.Test;
+import ru.job4j.dream.model.Post;
+import ru.job4j.dream.store.DbStore;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CandidateServletTest {
+
+    @Test
+    public void whenCreatePost() throws IOException, ServletException {
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        when(req.getParameter("id")).thenReturn("0");
+        when(req.getParameter("name")).thenReturn("name of new post");
+        new PostServlet().doPost(req, resp);
+        Post post = DbStore.instOf().findPostById(1);
+        assertThat(post, notNullValue());
+        assertThat(post.getName(), is("name of new post"));
+    }
+
+}
---
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.dream.servlet;
+
+import org.junit.Test;
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.store.DbStore;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class PostServletTest {
+
+    @Test
+    public void whenCreatePost() throws IOException, ServletException {
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        when(req.getParameter("id")).thenReturn("0");
+        when(req.getParameter("name")).thenReturn("name of new candidate");
+        new CandidateServlet().doPost(req, resp);
+        Candidate candidate = DbStore.instOf().findCandidateById(1);
+        assertThat(candidate, notNullValue());
+        assertThat(candidate.getName(), is("name of new candidate"));
+    }
+}
